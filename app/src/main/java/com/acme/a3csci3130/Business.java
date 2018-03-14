a41ec0333e7fe990754e7957c70177aa3049a796
Ryan Amaral <ryan_amaral@live.com>
2018-03-13 21:50:35 -0300

    added javadocs to business and added gservices json

--- app/src/main/java/com/acme/a3csci3130/Business.java
+++ app/src/main/java/com/acme/a3csci3130/Business.java
@@ -11,7 +11,6 @@ import java.util.Map;
  * Class that defines how the data will be stored in the
  * Firebase databse. This is converted to a JSON format
  */
-
 public class Business implements Serializable {
 
     public String businessNumber;
@@ -20,10 +19,21 @@ public class Business implements Serializable {
     public String address;
     public String provinceOrTerritory;
 
+    /**
+     * Default constructor required for calls to DataSnapshot.getValue.
+     */
     public Business() {
         // Default constructor required for calls to DataSnapshot.getValue
     }
 
+    /**
+     * Creates a business object.
+     * @param businessNumber Identifier of the business.
+     * @param name Name of the business.
+     * @param primaryBusiness The primary type of business that this business does.
+     * @param address The street address of the business.
+     * @param provinceOrTerritory The province/territory of this business.
+     */
     public Business(String businessNumber, String name, String primaryBusiness,
                     String address, String provinceOrTerritory){
         this.businessNumber = businessNumber;
@@ -33,6 +43,10 @@ public class Business implements Serializable {
         this.provinceOrTerritory = provinceOrTerritory;
     }
 
+    /**
+     * Maps the business object.
+     * @return Mapped business object.
+     */
     @Exclude
     public Map<String, Object> toMap(){
         HashMap<String, Object> result = new HashMap<>();
